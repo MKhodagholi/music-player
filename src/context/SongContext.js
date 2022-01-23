618899564f93b11743b9e3075cdf2a9c6b88e461
MKhodagholi <MKhodaagholi@gmail.com>
2022-01-23 10:34:10 +0330

    "add" state into SongProvider

--- src/context/SongContext.js
+++ src/context/SongContext.js
@@ -1,13 +1,19 @@
 import { createContext } from "react";
 
+import { songs } from "../asset/utils";
+
 const SongContext = createContext({
   songs: [],
   currentSong: {},
+  setCurrentSong: () => {},
 });
 
-const SongProvider = () => {
+const SongProvider = ({ children }) => {
+  const totalSongs = songs;
+  const [currentSong, setCurrentSong] = useState(totalSongs[0]);
+  const value = { songs: totalSongs, currentSong, setCurrentSong };
 
-  const value={};
+  return <SongContext.Provider value={value}>{children}</SongContext.Provider>;
+};
 
-  return <SongContext.Provider value={}></SongContext.Provider>
-}
+export { SongProvider, SongContext };
