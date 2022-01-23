85468def1bae3a1224d1677d124e8376f8ae15ee
MKhodagholi <MKhodaagholi@gmail.com>
2022-01-23 14:38:32 +0330

    "coding" Library Component

--- src/components/library/Library.jsx
+++ src/components/library/Library.jsx
@@ -1,7 +1,11 @@
 import React from "react";
+import { useContext } from "react";
+import { SongContext } from "../../context/SongContext";
 import LibrarySong from "./LibrarySong";
 
 const Library = () => {
+  const context = useContext(SongContext);
+  const { songs } = context;
   return (
     <div className="library">
       <h1>Library</h1>
