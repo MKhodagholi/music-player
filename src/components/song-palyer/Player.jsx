5ab8c01e4eaffea785092e06fe344ccf8c1f576d
MKhodagholi <MKhodaagholi@gmail.com>
2022-01-23 09:22:33 +0330

    "coding structure" of Player Component

---
+++ src/components/song-palyer/Player.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Plyaer = () => {
+  return (
+    <div>
+      <h1>Skip-back</h1>
+      <h2>Play</h2>
+      <h3>Skip-forward</h3>
+    </div>
+  );
+};
+
+export default Player;
