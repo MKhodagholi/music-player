193780c93949042ffc43a62260f159edb0c63a00
MKhodagholi <MKhodaagholi@gmail.com>
2022-01-23 14:47:34 +0330

    "add" Logic for play selected Music

--- src/components/library/LibrarySong.jsx
+++ src/components/library/LibrarySong.jsx
@@ -1,8 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
+
+import { SongContext } from "../../context/SongContext";
 
 const LibrarySong = ({ song }) => {
+  const context = useContext(SongContext);
+  const { isPlaying, audioRef, setCurrentSong } = context;
+
+  const clickSongHandler = (song) => {
+    setCurrentSong(song);
+    if (isPlaying) {
+      const playPromise = audioRef.current.play();
+      if (playPromise !== undefined) {
+        playPromise.then((audio) => {
+          audioRef.current.play();
+        });
+      }
+    }
+  };
+
   return (
-    <div className="library-song">
+    <div className="library-song" onClick={() => clickSongHandler(song)}>
       <img src={song.cover} />
       <div className="song-description">
         <h3>{song.name}</h3>
