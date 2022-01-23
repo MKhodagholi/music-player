c68502f982cf312506dae46f113d0f047ce4fe63
MKhodagholi <MKhodaagholi@gmail.com>
2022-01-23 10:06:28 +0330

    "Correct" Icon for Player Component

--- src/components/song-palyer/Player.jsx
+++ src/components/song-palyer/Player.jsx
@@ -1,5 +1,10 @@
 import React from "react";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay } from "@fortawesome/free-solid-svg-icons";
+import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
+import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
+
 const Player = () => {
   return (
     <div className="player">
@@ -9,9 +14,13 @@ const Player = () => {
         <p>End Time</p>
       </div>
       <div className="play-control">
-        <h1>Skip-back</h1>
-        <h2>Play</h2>
-        <h3>Skip-forward</h3>
+        <FontAwesomeIcon className="skip-back" icon={faAngleLeft} size="2x" />
+        <FontAwesomeIcon className="play" icon={faPlay} size="2x" />
+        <FontAwesomeIcon
+          className="skip-forward"
+          icon={faAngleRight}
+          size="2x"
+        />
       </div>
     </div>
   );
