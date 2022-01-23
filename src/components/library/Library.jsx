ae972016b525f5ac65d6aaa880f7dc8ea75fbd08
MKhodagholi <MKhodaagholi@gmail.com>
2022-01-23 14:38:09 +0330

    "coding" Library Component

--- src/components/library/Library.jsx
+++ src/components/library/Library.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import LibrarySong from "./LibrarySong";
 
 const Library = () => {
-  return <div>Library</div>;
+  return (
+    <div className="library">
+      <h1>Library</h1>
+      <div>
+        {songs.map((song) => (
+          <LibrarySong key={song.id} song={song} />
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Library;
