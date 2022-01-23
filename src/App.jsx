72641995221df7cf06de8565fda29811b0d6e777
MKhodagholi <MKhodaagholi@gmail.com>
2022-01-23 13:55:06 +0330

    "add" SongProvider into App Component

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,16 @@ import Song from "./components/song-palyer/Song";
 import Player from "./components/song-palyer/Player";
 
 import "./styles/app.scss";
+import { SongProvider } from "./context/SongContext";
 
 function App() {
   return (
-    <div className="app">
-      <Song />
-      <Player />
-    </div>
+    <SongProvider>
+      <div className="app">
+        <Song />
+        <Player />
+      </div>
+    </SongProvider>
   );
 }
 
