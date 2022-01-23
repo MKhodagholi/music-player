3d6b6913c7421d3d017a8766e1c2123195aa4339
MKhodagholi <MKhodaagholi@gmail.com>
2022-01-23 16:25:24 +0330

    "add" logic for library open or close

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,12 +2,23 @@ import React from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMusic } from "@fortawesome/free-solid-svg-icons";
+import { useContext } from "react";
+import { SongContext } from "../../context/SongContext";
 
 const Nav = () => {
+  const context = useContext(SongContext);
+
+  const { setLibraryIsOpen, libraryIsOpen } = context;
+
+  const libraryOnClickHandler = () => {
+    setLibraryIsOpen((prevState) => !prevState);
+    console.log(libraryIsOpen);
+  };
+
   return (
     <nav>
       <h1>Waves</h1>
-      <button>
+      <button onClick={libraryOnClickHandler}>
         Library <FontAwesomeIcon icon={faMusic} />
       </button>
     </nav>
