dd1dfade7d94852ed913e7a24c9eb095d7bc7bfd
MKhodagholi <MKhodaagholi@gmail.com>
2022-01-23 09:41:04 +0330

    "Correct" the structe the Player Component

--- src/components/song-palyer/Player.jsx
+++ src/components/song-palyer/Player.jsx
@@ -2,10 +2,17 @@ import React from "react";
 
 const Player = () => {
   return (
-    <div>
-      <h1>Skip-back</h1>
-      <h2>Play</h2>
-      <h3>Skip-forward</h3>
+    <div className="player">
+      <div className="time-control">
+        <p>Start Time</p>
+        <input type="range" />
+        <p>End Time</p>
+      </div>
+      <div className="play-control">
+        <h1>Skip-back</h1>
+        <h2>Play</h2>
+        <h3>Skip-forward</h3>
+      </div>
     </div>
   );
 };
