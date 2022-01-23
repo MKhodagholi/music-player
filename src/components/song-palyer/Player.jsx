b10142967b626e843a75a14ba4b75ad521629382
MKhodagholi <MKhodaagholi@gmail.com>
2022-01-23 13:53:15 +0330

    "add" context api and "add" play function

--- src/components/song-palyer/Player.jsx
+++ src/components/song-palyer/Player.jsx
@@ -1,11 +1,24 @@
 import React from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPlay } from "@fortawesome/free-solid-svg-icons";
+import { faPause, faPlay } from "@fortawesome/free-solid-svg-icons";
 import { faAngleLeft } from "@fortawesome/free-solid-svg-icons";
 import { faAngleRight } from "@fortawesome/free-solid-svg-icons";
+import { useContext } from "react";
+import { SongContext } from "../../context/SongContext";
 
 const Player = () => {
+  const context = useContext(SongContext);
+  const { changeIsPlaying, audioRef, isPlaying } = context;
+  const playHandler = () => {
+    if (isPlaying) {
+      audioRef.current.pause();
+    } else {
+      audioRef.current.play();
+    }
+    changeIsPlaying();
+  };
+
   return (
     <div className="player">
       <div className="time-control">
@@ -15,7 +28,12 @@ const Player = () => {
       </div>
       <div className="play-control">
         <FontAwesomeIcon className="skip-back" icon={faAngleLeft} size="2x" />
-        <FontAwesomeIcon className="play" icon={faPlay} size="2x" />
+        <FontAwesomeIcon
+          onClick={playHandler}
+          className="play"
+          icon={isPlaying ? faPause : faPlay}
+          size="2x"
+        />
         <FontAwesomeIcon
           className="skip-forward"
           icon={faAngleRight}
